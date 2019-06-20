51c60591ff8f2662eb571cb4188abb7291ba2d35
Aniruddha <aniruddha.chitley@dal.ca>
2019-06-20 13:51:53 -0300

    Implemented User Login

--- src/main/java/com/dal/mycareer/controller/RequestController.java
+++ src/main/java/com/dal/mycareer/controller/RequestController.java
@@ -14,12 +14,50 @@ public class RequestController {
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping("/homepage")
-	public String loadHome(Model model,HttpServletRequest request) {
-		
+	public String loadHome(Model model, HttpServletRequest request) {
+
 		LOGGER.info("Routed to Homepage");
 
 		return "homepage";
 	}
 
+	@RequestMapping("/studentHome")
+	public String studentHome(Model model, HttpServletRequest request) {
+
+		LOGGER.info("Routed to StudentHome");
+
+		if (request.getSession().getAttribute("sessionName") != null) {
+			return "studentHome";
+		} else {
+			return "redirect:/homepage";
+		}
+
+	}
+
+	@RequestMapping("/employerHome")
+	public String employerHome(Model model, HttpServletRequest request) {
+
+		LOGGER.info("Routed to EmployerHome");
+
+		if (request.getSession().getAttribute("sessionName") != null) {
+			return "employerHome";
+		} else {
+			return "redirect:/homepage";
+		}
+
+	}
+
+	@RequestMapping("/adminHome")
+	public String adminHome(Model model, HttpServletRequest request) {
+
+		LOGGER.info("Routed to AdminHome");
+
+		if (request.getSession().getAttribute("sessionName") != null) {
+			return "adminHome";
+		} else {
+			return "redirect:/homepage";
+		}
+
+	}
+
 }
- 	
