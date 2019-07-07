baee81adebb8056082adaad37e3e3c39baad8477
SarmadNoor <43487713+SarmadNoor@users.noreply.github.com>
2019-07-07 04:51:02 -0300

    controller code for registered student

--- src/main/java/com/dal/mycareer/controller/ManageStudentController.java
+++ src/main/java/com/dal/mycareer/controller/ManageStudentController.java
@@ -5,8 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import com.dal.mycareer.DTO.Student;
 import com.dal.mycareer.imodel.IManageStudentModel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +21,14 @@ public class ManageStudentController
     @Autowired
     IManageStudentModel manageStudentModel;
 
+    static Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @RequestMapping("/students")
+	public String getRegisteredStudents(ModelMap model)
+	{
+		return "students";
+    }
+    
     @ResponseBody
 	@RequestMapping( value="/registerstudent", method=RequestMethod.POST)
     public Student RegisterStudent(@RequestBody Student studentDetails, HttpServletRequest request)
