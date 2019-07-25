39b8d960720e1bddec04033653e06a9e21027777
Abinaya Raja <abinayaraja1411@gmail.com>
2019-07-25 02:13:31 -0300

    jobsmodel test

---
+++ src/test/java/com/dal/mycareer/controller/JobsModelTest.java
@@ -0,0 +1,29 @@
+package com.dal.mycareer.controller;
+
+import com.dal.mycareer.DAO.Impl.JobsDAO;
+import com.dal.mycareer.DAO.Interface.IJobsDAO;
+import com.dal.mycareer.imodel.IJobsModel;
+import com.dal.mycareer.model.JobsModel;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JobsModelTest
+{
+    IJobsDAO jobsMockDAO;
+    IJobsModel jobsModel;
+
+    @Before
+    public void setUp ()
+    {
+        jobsMockDAO = new JobsDAO();
+        jobsModel = new JobsModel(jobsMockDAO);
+    }
+
+    @Test
+    public void updateJobStatus()
+    {
+        
+    }
+}
