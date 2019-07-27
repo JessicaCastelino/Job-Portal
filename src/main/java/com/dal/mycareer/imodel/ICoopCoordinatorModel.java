a02b44865f4a9a20eb464725832a3de0b9605680
Jessica Castelino <js519934@dal.ca>
2019-07-27 16:58:01 -0300

    Unit test cases for CoopCoordinator

--- src/main/java/com/dal/mycareer/imodel/ICoopCoordinatorModel.java
+++ src/main/java/com/dal/mycareer/imodel/ICoopCoordinatorModel.java
@@ -6,13 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
 
+import com.dal.mycareer.DAO.Interface.ICoopCordinatorDAO;
 import com.dal.mycareer.DTO.RecruiterRequest;
+import com.dal.mycareer.emailengine.IEmployerApprovalEmail;
+import com.dal.mycareer.emailengine.IEmployerRejectionEmail;
+import com.dal.mycareer.passwordgenerator.IPasswordGenerator;
 
 public interface ICoopCoordinatorModel 
 {
-	public Model fetchRecruiterRequests(Model model, HttpServletRequest request);
-	public Model approveRecruiterRequest(Model model, HttpServletRequest request, int recruiterRequestId);
-	public Model rejectRecruiterRequest(Model model, HttpServletRequest request, int recruiterRequestId);
+	public Model fetchRecruiterRequests(Model model, HttpServletRequest request, ICoopCordinatorDAO coopCordinatorDAO);
+	public Model approveRecruiterRequest(Model model, HttpServletRequest request, int recruiterRequestId, ICoopCordinatorDAO coopCordinatorDAO, IEmployerApprovalEmail approvalEmail, IPasswordGenerator passwordGenerator);
+	public Model rejectRecruiterRequest(Model model, HttpServletRequest request, int recruiterRequestId, ICoopCordinatorDAO coopCordinatorDAO, IEmployerRejectionEmail rejectEmail);
 	public boolean deleteActiveRecruiter(int employerId);
 	public List<RecruiterRequest> fetchActiveRecruiters();
 	
