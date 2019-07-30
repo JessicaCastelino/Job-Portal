c4e20ff19a7d21eff359a7e6e1b73c8b45ea4096
Abinaya Raja <abinayaraja1411@gmail.com>
2019-07-30 13:09:25 -0300

    student list issue

--- src/main/java/com/dal/mycareer/JDBC/SelectHandler.java
+++ src/main/java/com/dal/mycareer/JDBC/SelectHandler.java
@@ -12,49 +12,65 @@ import com.dal.mycareer.DTOMapper.IDTOMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SelectHandler extends JdbcManager {
+public class SelectHandler extends JdbcManager 
+{
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private Map<String, Integer> procResult;
 
-	public SelectHandler() {
+	public SelectHandler() 
+	{
 		procResult = new HashMap<>();
 	}
 
 	@Override
 	public Map<String, Integer> executeProc(CallableStatement callStatement, String mapperObjectName, Object dtoObject,
-			Map<String, Object> additionalParam) {
+			Map<String, Object> additionalParam) 
+	{
 		ResultSet result = null;
-		try {
+		try 
+		{
 			ProcedureParamLoader paramLoader = new ProcedureParamLoader();
 			paramLoader.fillInputParameters(callStatement, additionalParam);
 			result = callStatement.executeQuery();
 			if (result !=  null)
 			{
-			fillDTOObjectFromResultSet(mapperObjectName, result, dtoObject);
-			procResult.put("isObjectFilled", 1);
+				fillDTOObjectFromResultSet(mapperObjectName, result, dtoObject);
+				procResult.put("isObjectFilled", 1);
 			}
-		} catch (Exception ex) {
+		} 
+		catch (Exception ex) 
+		{
 			logger.error("Error Occurred in executeProc :" + ex.getMessage());
-		} finally {
+		} 
+		finally 
+		{
 			DatabaseConnection.closeDatabaseComponents(result, callStatement, null);
 		}
 		return procResult;
 	}
 
-	private void fillDTOObjectFromResultSet(String mapperObjectName, ResultSet result, Object dtoObject) {
-		if (dtoObject != null) {
+	private void fillDTOObjectFromResultSet(String mapperObjectName, ResultSet result, Object dtoObject) 
+	{
+		if (dtoObject != null) 
+		{
 			IDTOMapper mapper = DTOMapper.dtoMap.get(mapperObjectName);
 			mapper.mapStatementtoObject(result, dtoObject);
-		} else {
-			try {
-				if (result.next()) {
+		} 
+		else 
+		{
+			try 
+			{
+				if (result.next()) 
+				{
 					procResult.put("recordExist", 1);
 				}
 				else
 				{
 					procResult.put("recordExist", 0);
 				}
-			} catch (Exception ex) {
+			} 
+			catch (Exception ex) 
+			{
 				logger.error("Error Occurred in fillDTOObjectFromResultSet :" + ex.getMessage());
 			}
 
