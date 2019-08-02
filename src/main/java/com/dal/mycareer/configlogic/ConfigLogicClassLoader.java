97b9905f7ec26e7c9d660567e6776d456849565c
Aniruddha <aniruddha.chitley@dal.ca>
2019-08-01 21:35:08 -0300

    Refactoring

--- src/main/java/com/dal/mycareer/configlogic/ConfigLogicClassLoader.java
+++ src/main/java/com/dal/mycareer/configlogic/ConfigLogicClassLoader.java
@@ -6,21 +6,23 @@ import java.util.Properties;
 
 import com.dal.mycareer.propertiesparser.PropertiesParser;
 
-public final class ConfigLogicClassLoader {
+public class ConfigLogicClassLoader {
 
+	private static final String JOB_TYPE = "job_type";
 	private static final String PREREQUISITE = "prerequisite";
 	private static final String ALL = "all";
 	private static final Properties PROPERTY_MAP = PropertiesParser.getPropertyMap();
 
-	Map<String, FetchJobs> configMap = new HashMap<String, FetchJobs>();
-
-	public static String getJobClass() {
-		return PROPERTY_MAP.getProperty("job_type").toString();
-	}
+	private static final Map<String, FetchJobs> configMap = new HashMap<String, FetchJobs>();
 
 	public ConfigLogicClassLoader() {
 		configMap.put(ALL, new FetchAllJobs());
 		configMap.put(PREREQUISITE, new FetchPrerequsiteCourseJobs());
+	}
+
+	public String getJobClass() {
+		FetchJobs jobclass = configMap.get(PROPERTY_MAP.getProperty(JOB_TYPE).toString());
+		return jobclass.getJobProcedure();
 	}
 
 }
